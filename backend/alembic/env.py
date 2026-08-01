95a83429a7bde0226a46b24f52cd131d7ade70b4
saumya <geerjasaumya@gmail.com>
2026-08-01 18:17:19 +0530

    bycript and alembic changes

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -8,9 +8,13 @@ from sqlalchemy import pool
 from alembic import context
 
 from core.database import Base
+from core.config import settings
 import models
 
 config = context.config
+
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL.replace("%", "%%"))
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
